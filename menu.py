dafc52bd0d066dc33058c022dcda2595ba418a90
Alexandre <alemrod01@gmail.com>
2024-04-22 20:12:45 -0300

    feat(ap01): funcao menu feita

--- menu.py
+++ menu.py
@@ -12,3 +12,29 @@ def dividir(a, b):
         return a / b
     else:
         print("Não se pode dividir por 0")
+
+def menu():
+        print("Selecione a operação:")
+        print("1. Adição")
+        print("2. Subtração")
+        print("3. Multiplicação")
+        print("4. Divisão")
+        print("0. Sair")
+        while True:
+            escolha = int(input("Escolha a operação desejada: "))
+            if escolha != 0:
+                num1 = float(input(" Digite o primeiro número: "))
+                num2 = float(input(" Digite o segundo número: "))
+            if escolha == 1:
+                print(f'Foi escolhido a soma de {num1} e {num2}, assim ficando {somar(num1, num2)}')
+            elif escolha == 2:
+                print(f'Foi escolhido a subtração de {num1} e {num2}, assim ficando {subtrair(num1, num2)}')
+            elif escolha == 3:
+                 print(f'Foi escolhido a multiplicação de {num1} e {num2}, assim ficando {multiplicar(num1, num2)}')
+            elif escolha == 4:
+                print(f'Foi escolhido a divisão de {num1} e {num2}, assim ficando {dividir(num1, num2)}')
+            else:
+                print("Saindo do menu")
+                break
+
+menu()
